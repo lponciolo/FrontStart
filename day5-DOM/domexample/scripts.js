df21fdfd04cbc91a0ff47ba4b6419b529263c0fb
Leonardo <l.ponciolo@gmail.com>
2021-07-22 20:40:27 -0300

    solve challenge

---
+++ day5-DOM/domexample/scripts.js
@@ -0,0 +1,18 @@
+const nombre = "Leonardo";
+nombre.toUpperCase();
+console.log("hola");
+const numerosDelUnoAlNueve = [0,1,2,3,4,5,6,7,8,9]
+var dni = {
+    nombre: "Leonardo",
+    apellido: "Ponciolo",
+    edad: 30,
+    comidasPreferidas: {
+        comida1: "Canelones",
+        comida2: "Milanesas"
+    }
+}
+setTimeout(function(){
+    var element = document.getElementById("subtitulo");
+    console.log(element);
+    element.innerHTML = "la web del perro";
+},1000)
